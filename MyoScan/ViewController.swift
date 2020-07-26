0726a16942ccaf598663eb33f1ac7d5b185b6179
yatharthchhabra <yatharthchhabra@gmail.com>
2020-07-26 15:41:28 -0700

    yatharth edit

--- MyoScan/ViewController.swift
+++ MyoScan/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("YATHARTH's EDIT WORKED")
         // Do any additional setup after loading the view.
     }
 
