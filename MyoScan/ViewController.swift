f811bee48f1af8b632338df2578f3fe1f18f7b55
Aditya Sharma <adityasharma1709@gmail.com>
2020-07-26 13:30:08 -0700

    Initial Commit

---
+++ MyoScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyoScan
+//
+//  Created by Aditya Sharma on 7/26/20.
+//  Copyright © 2020 Aditya Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
